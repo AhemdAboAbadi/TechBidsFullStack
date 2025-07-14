433a7e0c221bd5fc9103dcdefba2e35c0c4f9fc1
AhemdAboAbadi <ahmed.m.aboabadi@gmail.com>
2025-07-14 12:36:01 +0300

    refactor: comment out production static file serving logic

--- server/src/app.js
+++ server/src/app.js
@@ -68,11 +68,11 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use("/api", router)
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(join(__dirname, "..", "client", "build")))
-  app.get("*", (req, res) => {
-    res.sendFile(join(__dirname, "..", "client", "build", "index.html"))
-  })
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(join(__dirname, "..", "client", "build")))
+//   app.get("*", (req, res) => {
+//     res.sendFile(join(__dirname, "..", "client", "build", "index.html"))
+//   })
+// }
 
 module.exports = {app, server}
